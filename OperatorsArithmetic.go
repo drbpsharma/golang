7edf5d7b38a208564945f7aa47a24c5fbf52e041
Dr B P Sharma <drbpsharma@outlook.com>
2020-03-16 20:41:23 +0530

    Create OperatorsArithmetic.go

---
+++ OperatorsArithmetic.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int=10
+	var b int=3
+	fmt.Println(a+b) // 13
+	fmt.Println(a-b) // 7
+	fmt.Println(a*b) // 30
+	fmt.Println(a/b) // 3 -- integer division
+	fmt.Println(a%b) // 1
+}
